3b2ba539c3a74caf9321200d2f03b72a031defd8
dongchuangyoubai <1213238708@qq.com>
2020-07-02 14:37:54 +0800

    1

---
+++ language_model/language_model.py
@@ -0,0 +1,8 @@
+import torch
+import random
+import zipfile
+
+with zipfile.ZipFile('jaychou_lyrics.zip') as zin:
+    with zin.open('jaychou_lyrics.txt') as f:
+        corpus_chars = f.read().decode('utf-8')
+print(corpus_chars[:40])
